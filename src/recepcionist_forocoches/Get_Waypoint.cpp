fcb6a658576112311ba4c4900b74c8d5ef364a12
Javier Izquierdo Hernandez <j.izquierdoh.2021@alumnos.urjc.es>
2023-04-21 12:47:36 +0200

    [Snapshot] Adding base files and base bt

---
+++ src/recepcionist_forocoches/Get_Waypoint.cpp
@@ -0,0 +1,49 @@
+// Copyright 2021 Intelligent Robotics Lab
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "recepcionist_forocoches/Get_Waypoint.hpp"
+
+namespace recepcionist_forocoches
+{
+
+using namespace std::chrono_literals;  // NOLINT
+
+Get_Waypoint::Get_Waypoint(
+  const std::string & xml_tag_name,
+  const BT::NodeConfiguration & conf)
+: BT::ActionNodeBase(xml_tag_name, conf)
+{
+  config().blackboard->get("node", node_);
+
+}
+
+void
+Get_Waypoint::halt()
+{
+}
+
+BT::NodeStatus
+Get_Waypoint::tick()
+{
+  if (status() == BT::NodeStatus::IDLE) {
+    start_time_ = node_->now();
+  }
+  return BT::NodeStatus::SUCCESS;
+}
+}  // namespace recepcionist_forocoches
+#include "behaviortree_cpp_v3/bt_factory.h"
+BT_REGISTER_NODES(factory)
+{
+  factory.registerNodeType<recepcionist_forocoches::Get_Waypoint>("Get_Waypoint");
+}
